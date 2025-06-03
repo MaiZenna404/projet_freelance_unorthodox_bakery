e6698773f8079a5a0711aa8efb527f91119c33eb
gamatrick <valentin@laflyfamily.com>
2025-06-03 15:50:52 +0200

    wip: adapted email to form

--- unorthodox_bakery/src/App.tsx
+++ unorthodox_bakery/src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
+import { Button} from './components/ui/button'
+import { ContactForm } from "./components/ButtonMail"
+
+
 
 function App() {
 
   return (
     <>
       <h1 className='text-red-800 font-semibold text-3xl'>TEST</h1>
+      <Button variant='destructive'>Test</Button>
+      <ContactForm />
+
     </>
   )
 }
