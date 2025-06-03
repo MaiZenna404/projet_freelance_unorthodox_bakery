28ecdc7f2f894229f7da48cf4bfc6f76e7d3cff1
mai <mai.than.222@gmail.com>
2025-06-03 09:25:20 +0200

    add: added Footer

---
+++ unorthodox_bakery/src/components/Footer.tsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+  return (
+    <footer className="w-full bg-white/5 backdrop-blur-sm flex align-center text-center py-8 pt-10">
+      {/* Footer content */}
+      <div className="w-full flex flex-col justify-center items-center gap-6">
+        {/* Social and contact links */}
+        <div className="flex flex-col items-center justify-center w-full max-w-4xl mx-auto">
+          <Link
+            to="https://instagram.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-black font-primary text-md tracking-widest hover:underline px-4 py-1"
+          >
+            Instagram
+          </Link>
+          <Link
+            to="mailto:ub.wagram@gmail.com"
+            className="text-black font-primary text-md tracking-widest hover:underline px-4 py-1"
+          >
+            ub.wagram@gmail.com
+          </Link>
+        </div>
+
+        {/* Address and hours */}
+        <div className="flex flex-row items-center w-full max-w-4xl mx-auto gap-2">
+          <p className="text-black font-primary text-md tracking-widest text-center ml-[20%]">
+            169, Avenue de Wagram, 75017 Paris
+          </p>
+          <div className="h-5 w-px bg-black/30 mx-4"></div>
+          <p className="text-black font-primary text-md tracking-widest text-center">
+            Ouvert tous les jours du lundi au dimanche de 6h00 à 20h00
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- unorthodox_bakery/src/components/HeroSection.tsx
+++ unorthodox_bakery/src/components/HeroSection.tsx
@@ -1,8 +1,10 @@
+import Footer from "./Footer";
+
 export default function HeroSection() {
     return (
       <>
         {/* Hero Section */}
-        <div className="flex flex-col items-center justify-center min-h-[60vh] pt-[18%] px-4 bg-gradient-to-b from-white/25 to-transparent">
+        <div className="flex flex-col items-center justify-center min-h-[60vh] pt-[18%] px-4 bg-gradient-to-b from-white/25 to-transparent mb-[3%]">
           <h1 className="font-bold tracking-widest w-full text-center text-3xl">
             PAINS AU LEVAIN, BON CAFÉ, PLAISIRS SALÉS & SUCRÉS
           </h1>
@@ -11,7 +13,9 @@ export default function HeroSection() {
             moment de la journée. <br /> Des produits frais 100% maison, préparés avec
             des ingrédients sourcés localement... <br /> et beaucoup d'amour
           </p>
-        </div>
+            </div>
+            <Footer />
+        {/* End Hero Section */}
       </>
     );
 }
